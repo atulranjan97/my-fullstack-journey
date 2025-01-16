5808022c114388443953fd99b5bd9c86a239c825
AtulRanjan97 <atul.ranjan098@gmail.com>
2025-01-17 01:23:02 +0530

    Adding tailwind code that I learnt in first tailwind lecture

---
+++ 5-Tailwind/2-setup-tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // darkMode: 'media',  // Default (automatic based on system preference)
+
+  darkMode: 'class',  // Enables class-based dark mode (Manual toggle with 'dark' class)
+};
